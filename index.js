97b0076ec34e116c4f0d272cf132c1590f9e30ae
migben <miguelangelben1990@gmail.com>
2020-02-24 15:02:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM is loaded.");
+    const p = document.querySelector("#text");
+    p.textContent = "This is really cool!"
+});
+
+console.log("Im first because I get along with index.js");
